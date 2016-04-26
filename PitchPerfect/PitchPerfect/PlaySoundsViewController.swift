f888638fb593253ba33e0b322a5c222da515ba96
Soheil Norouzi <soheil.nrz@gmail.com>
2016-04-26 13:34:15 -0700

    Initial commit

---
+++ PitchPerfect/PitchPerfect/PlaySoundsViewController.swift
@@ -0,0 +1,83 @@
+//
+//  PlaySoundsViewController.swift
+//  PitchPerfect
+//
+//  Created by soheiln on 4/26/16.
+//  Copyright © 2016 soheiln. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class PlaySoundsViewController: UIViewController, AVAudioRecorderDelegate {
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+
+    @IBAction func playSoundForButton(sender: UIButton) {
+        print("play sound button pressed")
+        switch(ButtonType(rawValue: sender.tag)!) {
+        case .Slow:
+            playSound(rate: 0.5)
+        case .Fast:
+            playSound(rate: 1.5)
+        case .Chipmunk:
+            playSound(pitch: 1000)
+        case .Vader:
+            playSound(pitch: -1000)
+        case .Echo:
+            playSound(echo: true)
+        case .Reverb:
+            playSound(reverb: true)
+        }
+        configureUI(.Playing)
+        
+    }
+    
+    @IBAction func stopButtonPressed(sender: AnyObject) {
+        print("stop sound button pressed")
+        stopAudio()
+    }
+    
+    
+    var recordedAudioURL: NSURL!
+    var audioFile: AVAudioFile!
+    var audioEngine: AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: NSTimer!
+    
+    enum ButtonType: Int { case Slow = 0, Fast, Chipmunk, Vader, Echo, Reverb }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("PlaySoundsViewController loaded")
+        setupAudio()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        configureUI(.NotPlaying)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+
+
+}
---
+++ PitchPerfect/PitchPerfect/RecordSoundViewController.swift
@@ -0,0 +1,87 @@
+//
+//  ViewController.swift
+//  PitchPerfect
+//
+//  Created by soheiln on 4/25/16.
+//  Copyright (c) 2016 soheiln. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class RecordSoundViewController: UIViewController, AVAudioRecorderDelegate {
+    @IBOutlet weak var recordLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    
+    var audioRecorder: AVAudioRecorder!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        recordLabel.text = "Tap to record"
+        stopButton.enabled = false
+        recordButton.enabled = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+    @IBAction func startRecording(sender: AnyObject) {
+        print("record button pressed!")
+        recordLabel.text = "Stop recording"
+        recordButton.enabled = false
+        stopButton.enabled = true
+        
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentationDirectory, .UserDomainMask, true)[0] as String
+        
+        let recordingName = "recordedAudio.wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        print(filePath)
+        
+        let session = AVAudioSession.sharedInstance()
+        try! session.setCategory(AVAudioSessionCategoryPlayAndRecord)
+        
+        try! audioRecorder = AVAudioRecorder(URL: filePath!, settings: [:])
+        audioRecorder.delegate = self
+        audioRecorder.meteringEnabled = true
+        print(audioRecorder.prepareToRecord())
+        audioRecorder.record()
+    }
+    
+    @IBAction func stopRecording(sender: AnyObject) {
+        print("stop button pressed!")
+        recordLabel.text = "Tap to record"
+        stopButton.enabled = false
+        recordButton.enabled = true
+        audioRecorder.stop()
+        let audioSession = AVAudioSession.sharedInstance()
+        try! audioSession.setActive(false)
+    }
+    
+    func audioRecorderDidFinishRecording(recorder: AVAudioRecorder, successfully flag: Bool) {
+        print("AVAudioRecorder finished saving recording")
+        if (flag){
+            self.performSegueWithIdentifier("stopRecording", sender: audioRecorder.url)
+            
+        } else {
+            print("Saving of recording failed")
+        }
+    }
+    
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if (segue.identifier == "stopRecording") {
+            let playSoundsVC = segue.destinationViewController as! PlaySoundsViewController
+            let recordedAudioURL = sender as! NSURL
+            playSoundsVC.recordedAudioURL = recordedAudioURL
+            print("in preparefor segue")
+        }
+    }
+
+    
+}
+
